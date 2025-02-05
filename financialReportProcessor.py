5549eef2107e8ad7cbe9c1be381b74d28f91ee4d
Roby Eka Pramudita <mt-roby.pramudita@tech.jago.com>
2025-02-05 09:18:47 +0700

    add get path from args and update file naming

--- financialReportProcessor.py
+++ financialReportProcessor.py
@@ -1,3 +1,4 @@
+import argparse
 import pandas as pd
 import re
 import json
@@ -14,13 +15,14 @@ def parse_amount(amount_text):
 # Regex patterns for dates and payment method lines
 date_pattern = re.compile(r"(\d{2})/(\d{2})/(\d{4}), (\d{2}):(\d{2})")  # Matches dd/mm/yyyy
 payment_patterns = {
-    "BCA": re.compile(r"(?i)• BCA :\s*(\b\d{1,3}(?:\.\d{3})*(?:\s*rb)?\b)?\s*"),
-    "QRIS": re.compile(r"(?i)• QRIS :\s*(\b\d{1,3}(?:\.\d{3})*(?:\s*rb)?\b)?\s*"),
-    "CASH": re.compile(r"(?i)• CASH :\s*(\b\d{1,3}(?:\.\d{3})*(?:\s*rb)?\b)?\s*"),
-    "GOPAY": re.compile(r"(?i)• GOPAY :\s*(\b\d{1,3}(?:\.\d{3})*(?:\s*rb)?\b)?\s*")
+    "BCA": re.compile(r"(?i)•\s*BCA\s*:\s*(\b\d{1,3}(?:\.\d{3})*(?:\s*rb)?\b)?\s*"),
+    "QRIS": re.compile(r"(?i)•\s*QRIS\s*:\s*(\b\d{1,3}(?:\.\d{3})*(?:\s*rb)?\b)?\s*"),
+    "CASH": re.compile(r"(?i)•\s*CASH\s*:\s*(\b\d{1,3}(?:\.\d{3})*(?:\s*rb)?\b)?\s*"),
+    "GOPAY": re.compile(r"(?i)•\s*GOPAY\s*:\s*(\b\d{1,3}(?:\.\d{3})*(?:\s*rb)?\b)?\s*")
 }
 
 current_month = None
+yearList = set()
 
 # Function to process the chat data
 def process_chat_data(chat_file):
@@ -32,6 +34,7 @@ def process_chat_data(chat_file):
             date_match = date_pattern.search(line)
             if date_match:
                 day, month, year, hour, minute = date_match.groups()
+                yearList.add(year)
                 current_month = datetime(int(year), int(month), 1).strftime("%B %Y")
                 date_str = datetime(int(year), int(month), int(day), int(hour), int(minute)).strftime("%d %B %Y %H:%M")
 
@@ -52,21 +55,30 @@ def process_chat_data(chat_file):
 
 # Function to write data to Excel
 def write_to_excel(monthly_data):
-    writer = pd.ExcelWriter('test.xlsx')
+    writer = pd.ExcelWriter(f'report{yearList.pop()}.xlsx')
+    summary = defaultdict(lambda: defaultdict(int))
     for sheet,data in monthly_data.items():
         df = pd.DataFrame(data).T.iloc[:, [1, 2, 3, 4, 0]]
         sum = df.sum()
-        sum.name = 'Sum'
+        sum.name = 'Total'
         # Assign sum of all rows of DataFrame as a new row
         df = df._append(sum.transpose())
+        summary[sheet] = sum.to_dict()
         df.to_excel(writer, sheet_name=sheet,index=True)
+    summary_df = pd.DataFrame(summary).T
+    sum = summary_df.sum()
+    sum.name = 'Total'
+    # Assign sum of all rows of DataFrame as a new row
+    summary_df = summary_df._append(sum.transpose())
+    summary_df.to_excel(writer, sheet_name="summary", index=True)
     writer.close()
 
 # Main execution
 if __name__ == "__main__":
-    chat_file = '/Users/esha/Downloads/WhatsApp Chat with KEUANGAN MUSIKALITA.txt'  # Path to your exported WhatsApp chat file
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', dest='chatFile', type=str, help='Input chat file path')
+    args = parser.parse_args()
+    chat_file = args.chatFile  # Path to your exported WhatsApp chat file
     monthly_data = process_chat_data(chat_file)
-    with open("output.json", "w") as outfile:
-        json.dump(monthly_data, outfile)
     # print(monthly_data)
     write_to_excel(monthly_data)
